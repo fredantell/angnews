f857582d1d65f0d991673b467ed5f95879eaa2a0
Fredrik Antell <fred@fredantell.com>
2014-08-20 14:18:08 -0500

    Finish Ch5: add styles, add nav ctrler and view, add url filter

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.controller('NavCtrl', function($scope, $location, Post) {
+  $scope.post = {url: 'http://', title: ''};
+
+  $scope.submitPost = function () {
+    Post.create($scope.post).
+        then(function(ref) {
+          $scope.post = {url: 'http://', title: ''};
+          $location.path('/posts/' + ref.name());
+        });
+  };
+
+});
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -4,14 +4,6 @@ app.controller('PostCtrl', function($scope, $location, Post) {
   $scope.post = {url: 'http://', title: ''};
   $scope.posts = Post.all;
 
-  $scope.submitPost = function () {
-    Post.create($scope.post).
-        then(function(ref) {
-          $scope.post = {url: 'http://', title: ''};
-          $location.path('/posts/' + ref.name());
-        });
-  };
-
   $scope.deletePost = function(post) {
     Post.delete(post);
   };
