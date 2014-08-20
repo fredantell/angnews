e4bbed17173eeb8c5230e1825c4d9412ba1cfc67
Fredrik Antell <fred@fredantell.com>
2014-08-19 19:21:00 -0500

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angnewsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angnewsApp
+ */
+angular.module('angNewsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+        'LiveUpdate!'
+    ];
+  });
