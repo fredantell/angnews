eb88ec3b1af80a6f056c6f73d15dbf5b83a82778
Fredrik Antell <fred@fredantell.com>
2014-08-21 12:50:48 -0500

    Add Registration, Login, Logout.  ..docs were incorrect for createUser function

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('NavCtrl', function($scope, $location, Post) {
+app.controller('NavCtrl', function($scope, $location, Post, Auth) {
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
@@ -9,6 +9,14 @@ app.controller('NavCtrl', function($scope, $location, Post) {
           $scope.post = {url: 'http://', title: ''};
           $location.path('/posts/' + ref.name());
         });
+  };
+
+  $scope.signedIn = function() {
+    return Auth.signedIn();
+  };
+
+  $scope.logout = function() {
+    Auth.logout();
   };
 
 });
