cba935e112295897094bc5191bc2a9cd55dca3c3
Fredrik Antell <fred@fredantell.com>
2014-08-28 16:46:18 -0500

    Associate post with username

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('Post', function ($firebase, FIREBASE_URL) {
+app.factory('Post', function ($firebase, FIREBASE_URL, User) {
   var ref = new Firebase(FIREBASE_URL + 'posts');
 
   var posts = $firebase(ref).$asArray();
@@ -8,7 +8,22 @@ app.factory('Post', function ($firebase, FIREBASE_URL) {
   var Post = {
     all: posts,
     create: function(post) {
-      return posts.$add(post);
+      if (User.signedIn()) {
+        var user = User.getCurrent();
+        post.owner = user.username;
+
+        return posts.$add(post).then(function (ref) {
+          var postId = ref.name();
+
+          //not setting my currentUser to a ref like this line assumes.
+          //It's set to a snapshot of the user object in the backend under users/
+          var userRef = User.findByUsername(user.username);
+          var usersPosts = $firebase(userRef.child('posts'));
+          usersPosts.$set(postId, postId);
+          //user.$child('posts').$child(postId).$set(postId); // Will def break! :)
+          return postId;
+        });
+      }
     },
     find: function(postId) {
       return $firebase(ref.child(postId)).$asObject();
