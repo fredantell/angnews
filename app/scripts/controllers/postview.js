7790c6eb8f421682f7043dc940b0b26e25a66b6d
Fredrik Antell <fred@fredantell.com>
2014-09-01 20:50:16 -0500

    Add and delete comments.  Comments display and delete properly on page and in DB.

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -1,5 +1,13 @@
 'use strict';
 
-app.controller('PostViewCtrl', function($scope, $routeParams, Post) {
+app.controller('PostViewCtrl', function($scope, $location, $routeParams, Post, Comment) {
   $scope.post = Post.find($routeParams.postId).$asObject();
+  $scope.addComment = function() {
+    Comment.create($routeParams.postId, $scope.comment.text);
+    $scope.comment = '';
+  };
+  $scope.deleteComment = function(commentId) {
+    Comment.delete($routeParams.postId, commentId);
+    $location.path('/posts/' + $routeParams.postId);
+  };
 });
--- app/scripts/services/comment.js
+++ app/scripts/services/comment.js
@@ -3,9 +3,10 @@
 app.factory('Comment', function($firebase, User, Post) {
 
   function linkCommentToPostAndUser(postId, comment) {
+    var username = User.getCurrent().username;
     var postCommentsRef = Post.find(postId).$ref().child('comments');
-    var userCommentsRef = User.findByUsername(User.getCurrent().username).child('comments');
-    var commentObj = {comment: comment, postId: postId};
+    var userCommentsRef = User.findByUsername(username).child('comments');
+    var commentObj = {text: comment, postId: postId, username: username};
 
     return $firebase(postCommentsRef).$push(commentObj).then(function(ref) {
       //add uuid for comment to object
@@ -20,7 +21,26 @@ app.factory('Comment', function($firebase, User, Post) {
 
   var Comment = {
     create: function(postId, comment) {
+      if (!User.signedIn()) {return false;}  //only proceed if user is signed in
       linkCommentToPostAndUser(postId, comment);
+    },
+    delete: function(postId, commentId) {
+      //will need to delete the comment from the post as well as the user
+      //get postId from $routeParams
+      //get commentId from ng-repeat html anchor
+      var username = User.getCurrent().username;
+      var postCommentsRef = Post.find(postId).$ref().child('comments');
+      var userCommentsRef = User.findByUsername(username).child('comments');
+
+      return $firebase(postCommentsRef).$remove(commentId).then(function(ref) {
+        $firebase(userCommentsRef).$remove(commentId);
+      });
+
+
+
+      //will get owner from post.owner or getCurrent().username
+      //will ref.child('comments') and $remove commentId
+
     }
   };
 
